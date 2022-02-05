95d51b210674112f665675ac879e447704bffb0c
david <wizdave97@gmail.com>
2022-02-05 01:15:35 +0100

    add mmr batch proof and verification

--- primitives/arithmetic/src/fixed_point.rs
+++ primitives/arithmetic/src/fixed_point.rs
@@ -144,7 +144,7 @@ pub trait FixedPointNumber:
 		d: D,
 	) -> Option<Self> {
 		if d == D::zero() {
-			return None;
+			return None
 		}
 
 		let n: I129 = n.into();
@@ -437,7 +437,7 @@ macro_rules! implement_fixed {
 
 			fn saturating_pow(self, exp: usize) -> Self {
 				if exp == 0 {
-					return Self::saturating_from_integer(1);
+					return Self::saturating_from_integer(1)
 				}
 
 				let exp = exp as u32;
@@ -515,7 +515,7 @@ macro_rules! implement_fixed {
 		impl CheckedDiv for $name {
 			fn checked_div(&self, other: &Self) -> Option<Self> {
 				if other.0 == 0 {
-					return None;
+					return None
 				}
 
 				let lhs: I129 = self.0.into();
